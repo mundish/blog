b606f2897cb5c06100e2a2313a84e1f6073f2d27
Arvid Norlin <arvid.norlin@weahead.se>
2019-08-21 13:54:32 +0200

    feature: add blogpost content to blogpost template

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import React from "react"
+import { graphql } from "gatsby"
 
-const Template = (props) => {
-    return (<div>Blog post here</div>);
+const Template = ({ data: { markdownRemark } }) => {
+  const {
+    html,
+    frontmatter: { title },
+  } = markdownRemark
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <div className={"blogpost"} dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+  )
 }
 
-export default Template;
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
+
+export default Template
