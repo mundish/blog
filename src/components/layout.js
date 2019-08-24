d0870e3300e74114ac5004108b9f170d8ed05330
Arvid Norlin <arvidnorlin@gmail.com>
2019-08-24 18:31:37 +0200

    fix: shrink footer font size

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ export default ({ children }) => (
     <div className="m-0 max-w-2xl mx-auto p-5 min-h-screen flex flex-col">
       <Header />
       <div className={'flex-grow'}>{children}</div>
-      <footer className={'text-center pt-4 mt-8 border-t'}>
+      <footer className={'text-center pt-4 mt-8 border-t text-sm'}>
         Thanks for reading
       </footer>
     </div>
