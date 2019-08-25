5c4e8cca59ea5f1c4c066d0dbd6964cdbaed1560
Arvid Norlin <arvidnorlin@gmail.com>
2019-08-25 15:50:13 +0200

    chore: add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    cursor: {
+      auto: 'auto',
+      default: 'default',
+      pointer: 'pointer',
+      wait: 'wait',
+      text: 'text',
+      move: 'move',
+      help: 'help',
+    },
+  },
+  variants: {
+    visibility: ['group-hover'],
+  },
+};
