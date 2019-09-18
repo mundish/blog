97e1df044af67ce0fc970bf3384b30e697453f9a
Arvid Norlin <arvid.norlin@weahead.se>
2019-09-18 13:21:45 +0200

    refactor: rewrite Furigana props

--- src/components/Furigana.js
+++ src/components/Furigana.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Furigana = ({ children, reading }) => {
+const Furigana = ({ characters }) => {
   return (
-    <ruby className={'group cursor-help'}>
-      {children}
-      <rp>(</rp>
-      <rt className={'invisible group-hover:visible'}>{reading}</rt>
-      <rp>)</rp>
-    </ruby>
+    <>
+      {characters.map((char, idx) => {
+        return (
+          <ruby className={'group cursor-help'} key={`${char.kanji}${idx}`}>
+            {char.kanji}
+            <rp>(</rp>
+            <rt className={'invisible group-hover:visible'}>{char.kana}</rt>
+            <rp>)</rp>
+          </ruby>
+        );
+      })}
+    </>
   );
+};
+
+Furigana.propTypes = {
+  characters: PropTypes.arrayOf(
+    PropTypes.shape({
+      kanji: PropTypes.string.isRequired,
+      kana: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Furigana;
